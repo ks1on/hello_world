4105485d22f5a09433aee489740e3dcf76a5d7d2
Tihon <tihonrobotnik@gmail.com>
2024-06-18 14:42:02 +0300

    Edit hw

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
         System.out.println("Hello World!");
         System.out.println("Hello world 2");
         System.out.println("Hello world 3");
+        System.out.println("This is for pr");
     }
 }
